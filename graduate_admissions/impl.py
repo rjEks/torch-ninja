5f88f4d566c30857d709eedbefd273ddf0dba3f0
rjeks <rjekstein@gmail.com>
2022-09-14 23:00:13 -0300

    adjust lines and comments

--- graduate_admissions/impl.py
+++ graduate_admissions/impl.py
@@ -6,14 +6,18 @@ if __name__ == '__main__':
     net = Net(hidden_size=3, activation_fn='sigmoid', apply_dropout=False)
     result_3_sigmoid = Net.train_and_evaluate_model(net)
 
+    print("------------------------------------")
     net = Net(hidden_size=50, activation_fn='tanh')
     result_50_tanh = Net.train_and_evaluate_model(net)
 
+    print("------------------------------------")
     net = Net(hidden_size=1000, activation_fn='tanh')
     result_1000_tanh = Net.train_and_evaluate_model(net)
 
+    print("------------------------------------")
     net = Net(hidden_size=1000, activation_fn='tanh', apply_dropout=True)
     result_1000_tanh = Net.train_and_evaluate_model(net)
 
+    print("------------------------------------")
     net = Net(hidden_size=100, activation_fn='sigmoid')
     result_100_sigmoid = Net.train_and_evaluate_model(net)
